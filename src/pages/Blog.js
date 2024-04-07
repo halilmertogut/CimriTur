52172993f89c69029955741d0124cb7ee34553b6
mfatihkuru <fthkuru1903@gmail.com>
2024-04-07 23:05:11 +0300

    Update(CimriTur): Blog.js update

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -12,15 +12,15 @@ const Blog = () => {
     return (
         <div className="max-w-screen-xl mx-auto py-16 px-4 bg-white flex flex-col gap-6">
             <div className="flex flex-col items-center gap-4">
-                <img className=" h-auto  object-center " src="https://www.constantcontact.com/blog/wp-content/uploads/2020/02/Blog-7.png" alt="Blog Banner" />
+                <img className=" h-auto  object-center rounded" src="https://www.constantcontact.com/blog/wp-content/uploads/2020/02/Blog-7.png" alt="Blog Banner" />
                 <div className="text-center">
-                    <h1 className="text-5xl font-bold font-montserrat leading-snug text-black">Discover the Best Blogs</h1>
-                    <p className="text-lg font-normal font-montserrat text-black mt-2">Stay up to date with our latest blog posts.</p>
+                    <h1 className="text-5xl font-bold font-montserrat leading-snug text-black">En İyi Blogları Keşfedin</h1>
+                    <p className="text-lg font-normal font-montserrat text-black mt-2"></p>
                 </div>
             </div>
 
             <div className="flex justify-center gap-4">
-                {['View all', 'Technology', 'Health', 'Travel', 'Food'].map((category) => (
+                {['Hepsini Gör', 'Tarih', 'Macera', 'Doğa', 'Yemek'].map((category) => (
                     <div key={category}
                         className="px-4 py-2 border border-transparent rounded hover:border-gray-300 cursor-pointer"
                     >
@@ -29,13 +29,13 @@ const Blog = () => {
                 ))}
             </div>
 
-            <div className="flex flex-wrap justify-center gap-6">
+            <div className="flex flex-wrap justify-center gap-6 ">
                 {[1, 2, 3].map((item) => (
-                    <div key={item} className="flex flex-col items-center w-full sm:w-1/2 md:w-1/4" onClick={handleBlogClick}>
-                        <img className="w-full h-48 object-cover" src={`https://ntnc.org.np/sites/default/files/styles/slider_1920x1024/public/img_featured_thematic/2019-02/nature-base-tourism.jpg?itok=cZZ-Hkoi${item}`} alt={`Post ${item}`} />
+                    <div key={item} className="flex flex-col items-center w-full sm:w-1/2 md:w-1/4 " onClick={handleBlogClick}>
+                        <img className="w-full h-48 object-cover rounded" src={`https://ntnc.org.np/sites/default/files/styles/slider_1920x1024/public/img_featured_thematic/2019-02/nature-base-tourism.jpg?itok=cZZ-Hkoi${item}`} alt={`Post ${item}`} />
                         <div className="pt-4">
-                            <p className="text-sm font-semibold">Nature</p>
-                            <h2 className="text-xl font-bold">Explore the Wonders of South America</h2>
+                            <p className="text-sm font-semibold">Doğa </p>
+                            <h2 className="text-xl font-bold">Karadeniz'in Büyüleyici Doğasını Keşfedin!</h2>
                             <p className="mt-2 text-base font-normal">Experience the breathtaking landscapes, diverse wildlife, and vibrant cultures of South America.</p>
                             <div className="flex items-center gap-4 mt-4">
                                 <img className="w-12 h-12 rounded-full" src="https://www.nurses.co.uk/Images/Blog/author/b9e8a8fc-446d-4a24-84ae-416ed35e107f.png" alt="Author" />
@@ -52,7 +52,7 @@ const Blog = () => {
             <div className="flex flex-wrap justify-center gap-6">
                 {[1, 2, 3, 4].map((item) => (
                     <div key={item} className="flex flex-col items-center w-full sm:w-1/2 md:w-1/4 lg:w-1/5 "onClick={handleBlogClick}>
-                        <img className="w-full h-48 object-cover" src={`https://via.placeholder.com/302x148?text=Post+${item}`} alt={`Post ${item}`} />
+                        <img className="w-full h-48 object-cover rounded" src={`https://via.placeholder.com/302x148?text=Post+${item}`} alt={`Post ${item}`} />
                         <div className="pt-4">
                             <p className="text-sm font-semibold">Nature</p>
                             <h2 className="text-xl font-bold">Explore the Wonders of South America</h2>
