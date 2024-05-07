8cd9f538461a83330583c5e8c4f20e159dd37c92
Halil Mert Öğüt <101225219+halilmertogut@users.noreply.github.com>
2024-05-07 03:33:54 +0300

    refactor(cimri-tur): responsive ui

--- src/components/tour-list/TourCard.js
+++ src/components/tour-list/TourCard.js
@@ -1,56 +1,64 @@
 import React from 'react';
-import { StarIcon, LocationMarkerIcon, CashIcon, TruckIcon, GlobeAltIcon, InformationCircleIcon, ArrowNarrowRightIcon } from '@heroicons/react/outline';
+import {
+  StarIcon,
+  LocationMarkerIcon,
+  CashIcon,
+  TruckIcon,
+  GlobeAltIcon,
+  InformationCircleIcon,
+  ArrowNarrowRightIcon
+} from '@heroicons/react/outline';
 import DOMPurify from 'dompurify';
 
 const createMarkup = (htmlContent) => ({ __html: DOMPurify.sanitize(htmlContent) });
 
 const TourCard = ({ tour, onClick }) => (
-  <div 
-  className="bg-white max-w-lg w-[350px] rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:shadow-xl hover:-translate-y-1 hover:scale-105 cursor-pointer"
-  onClick={() => onClick(tour._id)}
->
-  <img src={tour.tourImagesUrl[0]} alt={tour.name} className="h-72 w-full object-cover" />
-  <div className="p-4">
-    <h2 className="text-xl font-bold text-gray-800 mb-2">{tour.name}</h2>
-    <p className="text-sm text-gray-600 mb-3" dangerouslySetInnerHTML={createMarkup(tour.description)}></p>
-      
-      <div className="flex items-center justify-between text-gray-600 mb-3">
-        <div className="flex items-center space-x-2">
-          <LocationMarkerIcon className="h-6 w-6 text-indigo-600" />
-          <span className="font-medium">{tour.startLocation}</span>
+  <div
+    className="bg-white rounded-lg overflow-hidden shadow-md transition duration-300 ease-in-out transform hover:shadow-xl hover:-translate-y-1 hover:scale-105 cursor-pointer flex flex-col"
+    onClick={() => onClick(tour._id)}
+    style={{ flex: "1 0 21%" }}  // Flex-grow, Flex-shrink, Flex-basis
+  >
+    <img src={tour.tourImagesUrl[0]} alt={tour.name} className="w-full object-cover" style={{ height: '200px' }} />
+    <div className="p-4 flex flex-col justify-between flex-grow">
+      <div>
+        <h2 className="text-lg font-bold text-gray-800 mb-2">{tour.name}</h2>
+        <p className="text-sm text-gray-600 mb-3" dangerouslySetInnerHTML={createMarkup(tour.description)} />
+        <div className="flex justify-between items-center mb-3">
+          <div className="flex items-center space-x-1">
+            <LocationMarkerIcon className="h-5 w-5 text-indigo-600" />
+            <span>{tour.startLocation}</span>
+          </div>
+          <ArrowNarrowRightIcon className="h-5 w-5" />
+          <div className="flex items-center space-x-1">
+            <LocationMarkerIcon className="h-5 w-5 text-green-600" />
+            <span>{tour.destination}</span>
+          </div>
         </div>
-        <ArrowNarrowRightIcon className="h-6 w-6 text-gray-700" />  {/* Centered Arrow Icon */}
-        <div className="flex items-center space-x-2">
-          <LocationMarkerIcon className="h-6 w-6 text-green-600" />
-          <span className="font-medium">{tour.destination}</span>
+        <div className="grid grid-cols-2 gap-1 text-sm">
+          <div className="flex items-center space-x-1">
+            <GlobeAltIcon className="h-5 w-5 text-blue-700" />
+            <span>{tour.region}</span>
+          </div>
+          <div className="flex items-center space-x-1">
+            <TruckIcon className="h-5 w-5 text-red-600" />
+            <span>{tour.transportType}</span>
+          </div>
+          <div className="flex items-center space-x-1">
+            <InformationCircleIcon className="h-5 w-5 text-blue-900" />
+            <span>{tour.type}</span>
+          </div>
         </div>
       </div>
-
-      <div className="grid grid-cols-2 gap-4 text-gray-600">
-        <div className="flex items-center space-x-2">
-          <GlobeAltIcon className="h-6 w-6 text-blue-700" />
-          <span className="font-medium">{tour.region}</span>
+      <div className="flex justify-between items-center mt-4">
+        <div className="flex items-center space-x-1">
+          <StarIcon className="h-5 w-5 text-yellow-600" />
+          <span>{tour.rating.toFixed(1)} Stars</span>
         </div>
-        <div className="flex items-center space-x-2">
-          <TruckIcon className="h-6 w-6 text-red-600" />
-          <span className="font-medium">{tour.transportType}</span>
+        <div className="flex items-center space-x-1">
+          <CashIcon className="h-5 w-5 text-green-600" />
+          <span>{tour.price} {tour.currency}</span>
         </div>
-        <div className="flex items-center space-x-2">
-          <InformationCircleIcon className="h-6 w-6 text-blue-900" />
-          <span className="font-medium">{tour.type}</span>
-        </div>
-      </div>
-
-      <div className="flex justify-between items-center mt-4 text-gray-700">
-        <div className="flex items-center space-x-2">
-          <StarIcon className="h-6 w-6 text-yellow-600" />
-          <span className="font-semibold">{tour.rating.toFixed(1)} Stars</span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <CashIcon className="h-6 w-6 text-green-600" />
-          <span className="font-semibold">{tour.price} {tour.currency}</span>
-        </div>
-        <button className="text-sm bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-lg transition-colors duration-300 ease-in-out ml-2">
+        <button className="text-xs bg-red-600 hover:bg-red-700 text-white py-1 px-2 rounded-lg">
           Daha Fazla
         </button>
       </div>
--- src/components/tour-list/TourList.js
+++ src/components/tour-list/TourList.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import TourCard from './TourCard';
-import FilterPanel from './FilterPanel';
-import { useLocation, useNavigate } from 'react-router-dom'; // Import useNavigate
+import React, { useState, useEffect } from "react";
+import TourCard from "./TourCard";
+import FilterPanel from "./FilterPanel";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const TourList = () => {
   const location = useLocation();
   const [tours, setTours] = useState([]);
   const [filteredTours, setFilteredTours] = useState([]);
   const [filters, setFilters] = useState(() => {
-    const defaultFilters = { type: '', region: '', rating: 'Hepsi', search: '', minPrice: '', maxPrice: '', startLocation: '' };
-    return location.state ? { ...defaultFilters, ...location.state } : defaultFilters;
-  });  const [currentPage, setCurrentPage] = useState(1);
+    const defaultFilters = {
+      type: "",
+      region: "",
+      rating: "Hepsi",
+      search: "",
+      minPrice: "",
+      maxPrice: "",
+      startLocation: "",
+    };
+    return location.state
+      ? { ...defaultFilters, ...location.state }
+      : defaultFilters;
+  });
+  const [currentPage, setCurrentPage] = useState(1);
   const [toursPerPage] = useState(9);
-  const navigate = useNavigate(); // Define navigate using useNavigate
-
-  const handleCardClick = (id) => {
-    navigate(`/explore/${id}`); // Use navigate to change the route
-  };
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTours = async () => {
-      const response = await fetch('http://localhost:3000/api/tours/all-tours');
+      const response = await fetch("http://localhost:3000/api/tours/all-tours");
       const data = await response.json();
       setTours(data);
       setFilteredTours(data);
@@ -29,70 +36,79 @@ const TourList = () => {
   }, []);
 
   useEffect(() => {
-    console.log(filters);
-      let result = tours.filter(tour =>
-        (!filters.type || tour.type.toLowerCase().trim() === filters.type.toLowerCase().trim()) &&
-        (!filters.region || tour.region.toLowerCase().trim() === filters.region.toLowerCase().trim()) &&
-        (!filters.rating || filters.rating === 'Hepsi' || tour.rating >= parseInt(filters.rating)) &&
-        (!filters.search || tour.name.toLowerCase().includes(filters.search.toLowerCase())) &&
-        (!filters.minPrice || tour.price >= parseFloat(filters.minPrice)) && 
+    let result = tours.filter(
+      (tour) =>
+        (!filters.type ||
+          tour.type.toLowerCase().trim() ===
+            filters.type.toLowerCase().trim()) &&
+        (!filters.region ||
+          tour.region.toLowerCase().trim() ===
+            filters.region.toLowerCase().trim()) &&
+        (!filters.rating ||
+          filters.rating === "Hepsi" ||
+          tour.rating >= parseInt(filters.rating)) &&
+        (!filters.search ||
+          tour.name.toLowerCase().includes(filters.search.toLowerCase())) &&
+        (!filters.minPrice || tour.price >= parseFloat(filters.minPrice)) &&
         (!filters.maxPrice || tour.price <= parseFloat(filters.maxPrice)) &&
-        (!filters.transportType || tour.transportType.toLowerCase().includes(filters.transportType.toLowerCase())) &&
-        (!filters.startLocation || tour.startLocation.toLowerCase().trim() === filters.startLocation.toLowerCase().trim())
+        (!filters.transportType ||
+          tour.transportType
+            .toLowerCase()
+            .includes(filters.transportType.toLowerCase())) &&
+        (!filters.startLocation ||
+          tour.startLocation.toLowerCase().trim() ===
+            filters.startLocation.toLowerCase().trim())
+    );
 
-      );
-    
-      if (filters.priceSort === 'asc') {
-        result.sort((a, b) => a.price - b.price);
-      } else if (filters.priceSort === 'desc') {
-        result.sort((a, b) => b.price - a.price);
-      }
+    if (filters.priceSort === "asc") {
+      result.sort((a, b) => a.price - b.price);
+    } else if (filters.priceSort === "desc") {
+      result.sort((a, b) => b.price - a.price);
+    }
     setFilteredTours(result);
   }, [filters, tours]);
-  
+
   const indexOfLastTour = currentPage * toursPerPage;
   const indexOfFirstTour = indexOfLastTour - toursPerPage;
   const currentTours = filteredTours.slice(indexOfFirstTour, indexOfLastTour);
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(filteredTours.length / toursPerPage); i++) {
     pageNumbers.push(i);
   }
 
-  const rows = [];
-  const numPerRow = 3; // number of tours per row
-  for (let i = 0; i < currentTours.length; i += numPerRow) {
-    rows.push(currentTours.slice(i, i + numPerRow));
-  }
-
   return (
-    <div className="container mx-auto px-4 flex flex-col md:flex-row mt-20 font-montserrat">
-      <div className="md:w-1/5 px-4 py-2">
+    <div className="container mx-auto px-4 flex flex-wrap md:flex-nowrap mt-20 font-montserrat">
+      <div className="w-full md:w-1/4 lg:w-1/5 mb-5 md:mb-0 px-4">
         <FilterPanel filters={filters} setFilters={setFilters} />
       </div>
-      <div className="md:w-4/5 px-4 py-2">
-        <div className="mb-12">
-          <h1 className="text-2xl font-bold text-gray-800">Turları Keşfet</h1>
-          <p className="text-gray-600 mt-2">
-            Tur listelerini tercihlerinize göre uyarlamak için soldaki filtreleri kullanın.
-          </p>
-        </div>
-        {rows.map((row, index) => (
-          <div className="flex justify-between gap-8 mb-8">
-            {row.map(tour => (
-              <TourCard 
+      <div className="w-full md:w-3/4 lg:w-4/5 px-4">
+        <h1 className="text-xl sm:text-2xl font-bold text-gray-800">
+          Turları Keşfet
+        </h1>
+        <p className="text-sm sm:text-base text-gray-600 mt-2 mb-5">
+          Tur listelerini tercihlerinize göre uyarlamak için soldaki filtreleri
+          kullanın.{" "}
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {currentTours.map((tour) => (
+            <TourCard
               key={tour._id}
-              tour={tour} 
-              onClick={() => handleCardClick(tour._id)} />
-            ))}
-          </div>
-        ))}
-        <nav className="flex justify-center mt-8">
-          <ul className="inline-flex items-center -space-x-px">
-            {pageNumbers.map(number => (
+              tour={tour}
+              onClick={() => navigate(`/explore/${tour._id}`)}
+            />
+          ))}
+        </div>
+        <nav className="mt-8 flex justify-center">
+          <ul className="flex list-none gap-2">
+            {pageNumbers.map((number) => (
               <li key={number}>
-                <button onClick={() => paginate(number)} className="px-4 py-2 leading-tight text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition-colors mr-4">
+                <button
+                  onClick={() => paginate(number)}
+                  className="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                >
                   {number}
                 </button>
               </li>
